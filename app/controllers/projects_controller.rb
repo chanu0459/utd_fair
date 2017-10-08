ae0884fca5e8ad6c435fbcc6fcbaf6a517953355
Chanukya <ychanukya@gmail.com>
2017-10-08 17:22:48 +0530

    date format fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,10 +2,9 @@ class ProjectsController < ApplicationController
   
   def index
 		@per_page = params[:per_page] || 2
-		@page = params[:page] || 0
+		@page = params[:page].to_i
 		@order_hash = ['Recent Projects', 'Order By Category Name ASC', 'Order By Username Asc', 'Order By Project Title Asc']
-  	@selected_order = (params[:sort_method].present? && @order_hash.include?(params[:sort_method])) ? params[:sort_method].parameterize.underscore : 'Recent Projects'.parameterize.underscore
-  	@result = Project.send(@selected_order)
+  	@result = Project.send((params[:sort_method] || "Recent Projects").to_s.parameterize.underscore)
 		@total_pages = @result.count / 2
 		@result = @result.limit(@per_page.to_i).offset( @per_page.to_i * @page.to_i)
   end
